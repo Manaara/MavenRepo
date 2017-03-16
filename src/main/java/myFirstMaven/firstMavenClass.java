70e1ae31df1232ad55096286e17c0d79ec380d64
user <user@10.63.10.245>
2017-03-16 09:04:18 +0200

    default

---
+++ src/main/java/myFirstMaven/firstMavenClass.java
@@ -0,0 +1,13 @@
+package myFirstMaven;
+
+public class firstMavenClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Maven");
+		
+		
+	}
+
+}
